2c7166cf77c249d088afff717e0fbf24cc891f06
Jon Lucas <jon.s.lucas@gmail.com>
2018-01-30 08:51:12 -0500

    test: Added default decks for testing UI.

--- reducers/index.js
+++ reducers/index.js
@@ -1,7 +1,31 @@
 import { RECEIVE_DECKS, ADD_CARD, ADD_DECK } from '../actions';
 
+const testDeck = {
+  React: {
+  title: 'React',
+  questions: [
+    {
+      question: 'What is React?',
+      answer: 'A library for managing user interfaces'
+    },
+    {
+      question: 'Where do you make Ajax requests in React?',
+      answer: 'The componentDidMount lifecycle event'
+    }
+  ]
+},
+JavaScript: {
+  title: 'JavaScript',
+  questions: [
+    {
+      question: 'What is a closure?',
+      answer: 'The combination of a function and the lexical environment within which that function was declared.'
+    }
+  ]
+}
+};
 
-export const decks = (state={}, action) => {
+export const decks = (state=testDeck, action) => {
   switch(action.type) {
     case RECEIVE_DECKS:
       return {
