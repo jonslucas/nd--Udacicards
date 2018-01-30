45b501858ced3ba74ac5a149b2cfeeca98c84b54
Jon Lucas <jon.s.lucas@gmail.com>
2018-01-30 08:52:39 -0500

    feat: Added basic DeckDetail component.

---
+++ components/DeckDetail.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Text } from 'react-native-elements';
+
+
+const DeckDetail = (props) => (
+  <View>
+    <Text h3>Deck Detail View</Text>
+  </View>
+);
+
+
+export default DeckDetail;
