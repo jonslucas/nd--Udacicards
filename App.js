9811886066815ee58b3553bd2faf36a0ecad1a7b
Jon Lucas <jon.s.lucas@gmail.com>
2018-01-30 08:53:37 -0500

    feat: Added Tab ans Stack Navigation to begin wiring transitions.

--- App.js
+++ App.js
@@ -1,17 +1,79 @@
 import React from 'react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Platform } from 'react-native';
+import { Constants } from 'expo';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
+import { purple, white } from './utils/colors';
 import { decks } from './reducers';
-import NewCard from './components/NewCard';
+import NewDeck from './components/NewDeck';
+import DeckList from './components/DeckList';
+import DeckDetail from './components/DeckDetail';
+
+const TopStatusBar = ({backgroundColor, ...props}) => (
+  <View style={{backgroundColor, height: Constants.statusBarHeight}}>
+    <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+  </View>
+);
+
+const Tabs = TabNavigator({
+  DeckList: {
+    screen: DeckList,
+    navigationOptions: {
+      tabBarLabel: 'Decks',
+      tabBarIcon: ({tintColor}) => <Ionicons name="ion-android-list" size={30} color={tintColor} />
+    },
+  },
+  NewDeck: {
+    screen: NewDeck,
+    navigationOptions: {
+      tabBarLabel: 'Add Deck',
+      tabBarIcon: ({tintColor}) => <FontAwesome name="plus-square" size={30} color={tintColor} />
+    },
+  },
+}, {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgba(0,0,0,0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+});
+
+const MainNav = StackNavigator({
+  Home: {
+    screen: Tabs,
+  },
+  DeckDetail: {
+    screen: DeckDetail,
+    navigationOptions: {
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: purple
+      }
+    }
+  }
+});
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(decks)} >
-        <View>
-          <Text>Hello World</Text>
-          <NewCard />
+        <View style={{flex: 1}}>
+          <TopStatusBar backgroundColor={purple} barStyle='light-content' />
+          <MainNav />
         </View>
 
       </Provider>
