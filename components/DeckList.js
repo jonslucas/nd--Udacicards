09599e771a5a6a1069f0db4ab88b72d3486750dc
Jon Lucas <jon.s.lucas@gmail.com>
2018-02-12 07:59:34 -0500

    feat: Added navigation functionality and altered the styling.

--- components/DeckList.js
+++ components/DeckList.js
@@ -2,18 +2,32 @@ import React, { Component } from 'react';
 import {View} from 'react-native';
 import { List, ListItem, Text } from 'react-native-elements';
 import { connect } from 'react-redux';
+import { NavigationActions } from 'react-navigation';
 
 class DeckList extends Component {
+  toDeckDetail = (title) => {
+    const navAct = NavigationActions.navigate({
+      routeName: 'DeckDetail',
+      params: {key: title},
+    });
+    this.props.navigation.dispatch(navAct);
+  }
   render() {
 
     const { decks } = this.props;
     const showDecks = decks.map((deck, ind)=>{
-      const subtitle = `${deck.questions.length} cards`;
+      const cardCount = deck.questions.length;
+
+      const cards = `${cardCount} ${cardCount===1?'card':'cards'}`;
       return (
         <ListItem
+          containerStyle={{height: 100}}
           key={ind}
-          title={deck.title}
-          subtitle={subtitle}
+          title={<Text h3> {deck.title}</Text>}
+          titleContainerStyle={{marginTop: 15}}
+          hideChevron
+          badge={{ value:cards, textStyle: {fontSize: 25}, containerStyle: {marginTop: 15} }}
+          onPress={()=>this.toDeckDetail(deck.title)}
         />
       );
 
