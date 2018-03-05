15416f278591cc5a78ba176bcd9bceba0ef50f32
Jon Lucas <jon.s.lucas@gmail.com>
2018-03-04 21:55:10 -0500

    feat: Fleshed out deck mgmt views/functions.

--- components/DeckDetail.js
+++ components/DeckDetail.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import { Text, Button } from 'react-native-elements';
 import { connect } from 'react-redux';
-
+import { NavigationActions } from 'react-navigation';
+import { deleteDeck } from '../actions';
+import { removeDeck } from '../utils/api';
 
 const DeckDetail = (props) => {
-  const { deck } = props;
+  const { deck, removeDeckFromState, navigation } = props;
   const cardCount = deck.questions.length;
   return (
     <View>
@@ -16,16 +18,45 @@ const DeckDetail = (props) => {
         raised
         icon={{name: 'page-multiple', type: 'foundation'}}
         title='Manage Cards'
+        onPress={()=>{
+          navigation.dispatch(NavigationActions.navigate({
+            routeName: 'CardList',
+            params: { deck: deck.title }
+          }));
+        }}
       />
       <Button
         raised
         icon={{name: 'page-add', type: 'foundation'}}
         title='Add Cards'
+        onPress={()=>{
+          navigation.dispatch(NavigationActions.navigate({
+            routeName: 'NewCard',
+            params: {deckTitle: deck.title}
+          }));
+        }}
       />
       <Button
         raised
         icon={{name: 'trophy', type: 'foundation'}}
         title='Quiz'
+        onPress={()=>{
+          navigation.dispatch(NavigationActions.navigate({
+            routeName: 'Quiz',
+          }));
+        }}
+      />
+      <Button
+        raised
+        icon={{name: 'x', type: 'foundation'}}
+        title='Delete'
+        onPress={()=>{
+          removeDeck(deck.title);
+          removeDeckFromState(deck.title);
+          navigation.dispatch(NavigationActions.navigate({
+            routeName: 'DeckList',
+          }));
+        }}
       />
 
     </View>
@@ -37,7 +68,12 @@ const stateToProps = (state, ownProps) => {
   const deck = ownProps.navigation.state.params.key;
   return {
     deck: state[deck],
+    ...ownProps,
   }
-}
+};
 
-export default connect(stateToProps)(DeckDetail);
+const dispToProps = dispatch => ({
+  removeDeckFromState: (deck)=>(dispatch(deleteDeck(deck))),
+});
+
+export default connect(stateToProps, dispToProps)(DeckDetail);
