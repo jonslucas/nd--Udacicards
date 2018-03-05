021b3c7837c3a50ece3c5b3d8dd761d3c3f18dcc
Jon Lucas <jon.s.lucas@gmail.com>
2018-03-04 21:54:32 -0500

    feat: Added deletion to redux.

--- actions/index.js
+++ actions/index.js
@@ -1,5 +1,7 @@
 export const ADD_DECK = 'ADD_DECK';
 export const ADD_CARD = 'ADD_CARD';
+export const DELETE_DECK = 'DELETE_DECK';
+export const DELETE_CARD = 'DELETE_CARD';
 export const RECEIVE_DECKS = 'RECEIVE_DECKS';
 
 
@@ -8,11 +10,26 @@ export const addDeck = (title) => (
     type: ADD_DECK,
     title,
   }
-)
+);
+
+export const deleteDeck = (title) => (
+  {
+    type: DELETE_DECK,
+    title,
+  }
+);
 
 export const addCard = (title, card) => (
   {
     type: ADD_CARD,
+    title,
+    card,
+  }
+);
+
+export const deleteCard = (title, card) => (
+  {
+    type: DELETE_CARD,
     title,
     card,
   }
--- reducers/index.js
+++ reducers/index.js
@@ -1,4 +1,4 @@
-import { RECEIVE_DECKS, ADD_CARD, ADD_DECK } from '../actions';
+import { RECEIVE_DECKS, ADD_CARD, ADD_DECK, DELETE_DECK, DELETE_CARD } from '../actions';
 
 const testDeck = {
   React: {
@@ -12,7 +12,8 @@ const testDeck = {
       question: 'Where do you make Ajax requests in React?',
       answer: 'The componentDidMount lifecycle event'
     }
-  ]
+  ],
+  toDelete: false,
 },
 JavaScript: {
   title: 'JavaScript',
@@ -21,17 +22,18 @@ JavaScript: {
       question: 'What is a closure?',
       answer: 'The combination of a function and the lexical environment within which that function was declared.'
     }
-  ]
+  ],
+  toDelete: false,
 }
 };
 
-export const decks = (state=testDeck, action) => {
+export const decks = (state={}, action) => {
   switch(action.type) {
     case RECEIVE_DECKS:
       return {
         ...state,
-        ...decks,
-      }
+        ...action.decks,
+      };
     case ADD_CARD:
       return {
         ...state,
@@ -39,13 +41,35 @@ export const decks = (state=testDeck, action) => {
           ...state[action.title],
           questions: [...state[action.title].questions, action.card],
         }
-      }
-    case ADD_DECK:
+      };
+    case DELETE_CARD:
       return {
         ...state,
         [action.title]: {
-          title: action.title,
-          questions: [],
+          ...state[action.title],
+          questions: state[action.title].questions
+                      .filter(q=>(
+                        q.question !== action.card.question &&
+                        q.answer !== action.card.answer
+                      )),
+        }
+      };
+    case ADD_DECK:
+      const deck = {
+        title: action.title,
+        questions: [],
+        toDelete: false,
+      };
+      return {
+        ...state,
+        [action.title]: deck,
+      };
+    case DELETE_DECK:
+      return {
+        ...state,
+        [action.title]: {
+          ...state[action.title],
+          toDelete: true,
         }
       };
     default:
