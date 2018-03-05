aa645f4ed8b4f55c8a1ecda21ffb3fc5d8b399c9
Jon Lucas <jon.s.lucas@gmail.com>
2018-03-04 21:56:12 -0500

    feat: Added delete support.

--- components/DeckList.js
+++ components/DeckList.js
@@ -3,8 +3,24 @@ import {View} from 'react-native';
 import { List, ListItem, Text } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { NavigationActions } from 'react-navigation';
+import { receiveDecks } from '../actions';
+import { loadDecks } from '../utils/api';
 
 class DeckList extends Component {
+  state = { ready: false }
+  componentDidMount() {
+    const { decks, decksToState } = this.props;
+
+    if (decks.length < 1) {
+      console.log('Calling loadDecks');
+      loadDecks()
+        .then(decksToState)
+        .then(_=>this.setState({ready:true}))
+        .catch(console.log);
+    }
+
+
+  }
   toDeckDetail = (title) => {
     const navAct = NavigationActions.navigate({
       routeName: 'DeckDetail',
@@ -17,7 +33,6 @@ class DeckList extends Component {
     const { decks } = this.props;
     const showDecks = decks.map((deck, ind)=>{
       const cardCount = deck.questions.length;
-
       const cards = `${cardCount} ${cardCount===1?'card':'cards'}`;
       return (
         <ListItem
@@ -31,20 +46,32 @@ class DeckList extends Component {
         />
       );
 
-    })
+    });
     return (
-      <List>
-        {
-          showDecks
-        }
-      </List>
+      <View>
+        <List>
+          { showDecks }
+        </List>
+
+      </View>
     );
   }
 }
-const stateTopProps = (state, ownProps) => (
-  {
-    decks: Object.keys(state).map(key=>state[key]),
+const stateToProps = (state, ownProps) => {
+  const decks = Object.keys(state)
+                .map(key=>state[key])
+                .filter(d=>{
+                  const { title, toDelete } = d;
+                  return d.toDelete === false;
+                });
+  return {
+    decks,
     ...ownProps,
   }
+};
+const dispToProps = dispatch => (
+  {
+    decksToState: (decks) => dispatch(receiveDecks(decks))
+  }
 );
-export default connect(stateTopProps)(DeckList);
+export default connect(stateToProps, dispToProps)(DeckList);
