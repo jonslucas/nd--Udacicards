16da4cd98b0600162a0f1d24dc08926762e4c49f
Jon Lucas <jon.s.lucas@gmail.com>
2018-03-04 21:59:51 -0500

    feat: Rename component to clarify expanded role.

--- components/CardDetail.js
+++ components/CardDetail.js
@@ -2,39 +2,50 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import { Text, FormLabel, FormInput, FormValidationMessage, Button } from 'react-native-elements';
 import { connect } from 'react-redux';
-import { addCard } from '../actions';
+import { addCard, deleteCard } from '../actions';
+import { saveToStorage } from '../utils/api';
 import { NavigationActions } from 'react-navigation';
 
-class NewCard extends Component {
+class CardDetail extends Component {
   state={
-    question: '',
-    answer: '',
+    question: this.props.prevQuestion || null,
+    answer: this.props.prevAnswer || null,
   }
   onSubmit(navToNext) {
 
-    const { submit, navigation } = this.props;
+    const { submit, navigation, prevQuestion, prevAnswer, remove } = this.props;
     const { deckTitle } = navigation.state.params;
     const { question, answer } = this.state;
     let navAction;
     if(navToNext === 'newCard') {
-      // TODO: Nav to NewCard.
       navAction = NavigationActions.navigate({
-        routeName: 'NewCard',
+        routeName: 'CardDetail',
         params: {deckTitle}
       });
     } else if (navToNext === 'deckList') {
-      // TODO: Nav to Deck List
       navAction = NavigationActions.navigate({
         routeName: 'DeckList'
       });
     }
-    submit(deckTitle, {question, answer});
+    if (question && answer ) {
+      remove(deckTitle, {
+        question: prevQuestion,
+        answer: prevAnswer,
+      });
+      submit(deckTitle, {question, answer});
+      saveToStorage({
+        title: deckTitle,
+        questions:  [{question, answer}],
+        toDelete: false,
+      });
+    }
     navigation.dispatch(navAction);
+
   }
   setAnswer = (answer)=>(this.setState({answer}))
   setQuestion = (question)=>(this.setState({question}))
   render() {
-    const { cardCount, navigation } = this.props;
+    const { cardCount, navigation, prevQuestion, prevAnswer } = this.props;
     const { deckTitle } = navigation.state.params;
     const countStr = `${cardCount} card${cardCount===1?'':'s'}`;
     return (
@@ -43,20 +54,26 @@ class NewCard extends Component {
         <Text h3 >{countStr}</Text>
         <View >
           <FormLabel>Question</FormLabel>
-          <FormInput ref={input=>this.question=input} onChangeText={this.setQuestion}/>
+          <FormInput
+            ref={input=>this.question=input}
+            onChangeText={this.setQuestion}
+            defaultValue={prevQuestion}
+          />
         </View>
         <View >
           <FormLabel>Answer</FormLabel>
-          <FormInput ref={input=>this.answer=input} onChangeText={this.setAnswer}/>
+          <FormInput
+            ref={input=>this.answer=input}
+            onChangeText={this.setAnswer}
+            defaultValue={prevAnswer}
+          />
         </View>
         <View style={{marginBottom: 10, marginTop: 20}}>
-          <Button title="Add Next Card" onPress={()=>this.onSubmit('newCard')} />
+          <Button title="Submit and Add New" onPress={()=>this.onSubmit('newCard')} />
         </View>
         <View style={{marginTop: 10}}>
           <Button title="Complete Deck" onPress={()=>this.onSubmit('deckList')} />
         </View>
-        {/* <Text h4>{JSON.stringify(this.state, null, 2)}</Text> */}
-        <Text h4>{JSON.stringify(this.props, null, 2)}</Text>
       </View>
     );
   }
@@ -64,14 +81,19 @@ class NewCard extends Component {
 
 
 const stateToProps = (state, ownProps) => {
-  const {deckTitle} = ownProps.navigation.state.params;
+  const {deckTitle, question, answer} = ownProps.navigation.state.params;
 
     const deck = state.hasOwnProperty(deckTitle) ? state[deckTitle] : {questions: []};
     return {
       ...ownProps,
-      cardCount: deck.questions.length
+      cardCount: deck.questions.length,
+      prevQuestion: question,
+      prevAnswer: answer,
     };
 }
-const dispatchToProps = (dispatch) => ({submit: (title, card)=>dispatch(addCard(title, card))});
+const dispatchToProps = (dispatch) => ({
+  submit: (title, card)=>dispatch(addCard(title, card)),
+  remove: (title, card)=>dispatch(deleteCard(title, card)),
+});
 
-export default connect(stateToProps, dispatchToProps)(NewCard);
+export default connect(stateToProps, dispatchToProps)(CardDetail);
