f4feb74e717b95f11529e8e9a0675d08bce65644
Jon Lucas <jon.s.lucas@gmail.com>
2018-03-07 00:11:32 -0500

    feat: Added component for seeing quiz results.

---
+++ components/QuizResults.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Text, Button } from 'react-native-elements';
+import { orange, green } from '../utils/colors';
+
+const Results = props => {
+
+    const { correctCount, navBack, restart } = props;
+
+    return (
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <Text h3>{correctCount} answered correctly</Text>
+        </View>
+        <View style={{flex: 1}} >
+          <Button
+            raised
+            title="Retake Quiz"
+            buttonStyle={{backgroundColor: green}}
+            onPress={restart}
+          />
+        </View>
+        <View style={{flex: 1}} >
+          <Button
+            raised
+            title="Back to Deck"
+            buttonStyle={{backgroundColor: orange}}
+            onPress={navBack}
+          />
+        </View>
+      </View>
+    );
+}
+
+export default Results;
